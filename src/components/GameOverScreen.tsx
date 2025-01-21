9bfe783231b98bf5745ec8960543b69e94128d2f
Thomas Kong <txdk2009@gmail.com>
2025-01-22 08:31:51 +1030

    Animate score on game over screen

--- src/components/GameOverScreen.tsx
+++ src/components/GameOverScreen.tsx
@@ -1,5 +1,6 @@
 import { getMonsterArticle } from "../core/MonsterGenerator";
 import NewGameButton from "./NewGameButton";
+import SlotCounter from "react-slot-counter";
 
 interface GameOverScreenProps {
     monsterName: string;
@@ -14,7 +15,10 @@ export default function GameOverScreen({ monsterName, score, startGame }: Readon
             <p className="font-customFont text-green-500 text-center mt-3">
                 Slain by {getMonsterArticle(monsterName)} {monsterName}...
             </p>
-            <p className="font-customFont text-green-500 text-center mt-3">FINAL SCORE: {score}</p>
+            <p className="font-customFont text-green-500 text-center mt-3">
+                FINAL SCORE:{" "}
+                <SlotCounter value={score} />
+            </p>
             <NewGameButton className="mt-[60px]" startGame={startGame} />
         </>
     );

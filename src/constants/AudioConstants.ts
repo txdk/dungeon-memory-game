b1716b374db33905ad041f7c3aed4089d91d31c0
Thomas Kong <txdk2009@gmail.com>
2025-02-02 20:11:04 +1030

    Remove debug commands in workflow

--- src/constants/AudioConstants.ts
+++ src/constants/AudioConstants.ts
@@ -1,4 +1,5 @@
-const BASE_SFX_PATH: string = "./dungeon-memory-game/sfx";
+const isProd: boolean = process.env.NODE_ENV === "production";
+const BASE_SFX_PATH: string = isProd? "./sfx": "./dungeon-memory-game/sfx";
 
 export const SELECT_SOUND: string = `${BASE_SFX_PATH}/select.wav`;
 export const HURT_SOUND: string = `${BASE_SFX_PATH}/hitHurt.wav`;
